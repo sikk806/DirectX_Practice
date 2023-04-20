61fd466f05ebb80d333b28300e9ace389f44fbb6
unknown <sikk806@gmail.com>
2023-04-20 18:43:20 +0900

    Vector Basic

---
+++ 1_Vector/DXMVector/DXMVector/XMath.cpp
@@ -0,0 +1,39 @@
+#include <Windows.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <iostream>
+
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+ostream& XM_CALLCONV operator<<(ostream& os, FXMVECTOR v) {
+	XMFLOAT3 dest;
+	XMStoreFloat3(&dest, v);
+
+	os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
+	return os;
+}
+
+int main(void) {
+	cout.setf(ios_base::boolalpha);
+
+	if (!XMVerifyCPUSupport()) {
+		cout << "DIrectXMath를 지원하지 않음." << endl;
+		return 0;
+	}
+
+	XMVECTOR p = XMVectorZero();
+	XMVECTOR q = XMVectorSplatOne();
+	XMVECTOR u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
+	XMVECTOR v = XMVectorReplicate(-2.0f);
+	XMVECTOR w = XMVectorSplatZ(u);
+
+	cout << "p = " << p << endl;
+	cout << "q = " << q << endl;
+	cout << "u = " << u << endl;
+	cout << "v = " << v << endl;
+	cout << "w = " << w << endl;
+
+	return 0;
+}

775d67e8cb899cc517a9117e09db7b42afa7fe4e
unknown <sikk806@gmail.com>
2023-06-05 17:39:56 +0900

    DX11

--- DX11/mineEngine/WindowsProject1/Applicationclass.h
+++ DX11/mineEngine/WindowsProject1/Applicationclass.h
@@ -3,7 +3,7 @@
 
 #include <windows.h>
 
-const bool FULL_SCREEN = false;
+const bool FULL_SCREEN = true;
 const bool VSYNC_ENABLED = true;
 const float SCREEN_DEPTH = 1000.0f;
 const float SCREEN_NEAR = 0.3f;
